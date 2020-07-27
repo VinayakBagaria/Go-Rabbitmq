5fba32d53373f309cbafff069b0c0e3009723a89
techentertainer <workvinayak95@gmail.com>
2020-07-27 13:02:09 +0530

    install amqp

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/VinayakBagaria/Go-Rabbitmq
 
 go 1.14
+
+require github.com/streadway/amqp v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
 
 func main() {
 	fmt.Println("Go RMQ")
