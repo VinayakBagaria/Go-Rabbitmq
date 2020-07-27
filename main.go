aeadba1edfc582ed088910f525b47cc053703690
techentertainer <workvinayak95@gmail.com>
2020-07-27 13:12:02 +0530

    publishing msg to queue

--- main.go
+++ main.go
@@ -16,4 +16,30 @@ func main() {
 	defer conn.Close()
 
 	fmt.Println("Successfully connected to RMQ")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare("TestQueue", false, false, false, false, nil)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(q)
+
+	msg := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("Hello World"),
+	}
+	err = ch.Publish("", "TestQueue", false, false, msg)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println("Successfully published msg to queue")
 }
